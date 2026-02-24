895a033c9a4d0797ead0636c34d2bd8c808f5b0b
blwatkins <blwatkins@users.noreply.github.com>
2026-02-24 12:41:19 -0600

    Add static files, p5.js sketches, and index.html page.

--- src/app.mjs
+++ src/app.mjs
@@ -3,9 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => {
-    response.send('Hello, World!');
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Application listening at port ${port}`);
