dee66ef0ec1136f02236760d44807709f51d7f0e
blwatkins <blwatkins@users.noreply.github.com>
2026-02-26 13:50:25 -0600

    Add ejs view for homepage.

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,20 @@
 import express from 'express';
+import ejs from 'ejs';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', (request, response) => {
+    response.render('index');
+});
+
+app.get('/random-color', (request, response) => {
+    response.send('hello, world!');
+});
 
 app.listen(port, () => {
     console.log(`Application listening at port ${port}`);
